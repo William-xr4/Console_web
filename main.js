7c29d523744b5603552d5d221a70acfee7629d45
William-xr4 <108703320+William-xr4@users.noreply.github.com>
2022-09-28 12:33:03 -0300

    Add files via upload
    hospedando meu site.

---
+++ main.js
@@ -0,0 +1,32 @@
+console.log("Meu primeiro projeto no console");
+var primeiro_nome = "William ";
+
+var segundo_nome = "da Silva";
+
+var ultimo_nome = " Leite";
+
+var nome_completo = primeiro_nome.concat(segundo_nome);
+
+nome_completo =nome_completo.concat(ultimo_nome);
+
+console.log(nome_completo);
+
+var x = 10;
+
+var y = 5.2;
+
+var soma = x+y;
+
+console.log("O valor da soma é " + soma);
+
+var subtracao = x-y;
+
+console.log("O valor da subtração é " + subtracao);
+
+var multiplicacao = x*y;
+
+console.log("O valor da multiplicação é " + multiplicacao)
+
+var divisao = x/y;
+
+console.log("O valor da divisão é " + divisao);
